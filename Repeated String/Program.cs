23dcad8837e404e616c7fb1a5173a8240d433a76
Shabar Mohajer-Iravani <shabar.mohaje@microsoft.com>
2019-08-26 11:40:41 -0400

    Add project files.

---
+++ Repeated String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Repeated_String
+{
+	class Program
+	{
+
+		/*
+		 * https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+		*/
+
+		static long repeatedString(string s, long n)
+		{
+			// counts all letter a's from string s via linq
+			int a = s.Count(x => x == 'a');
+
+			//divides n by s.Length to give repeat factor, the mulitples by a (but in decimal so we can round up/down if needed)
+			//returns in long (Int64)
+			return Convert.ToInt64(Math.Round(a * (n / (decimal)s.Length)));
+
+		}
+		static void Main(string[] args)
+		{
+			long test = Program.repeatedString("aba", 10);
+			Console.WriteLine(test);
+			Console.ReadKey();
+		}
+	}
+}
